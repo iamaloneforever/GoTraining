0e76565fbce7f2bcd653c53cf6fa05e2f5997219
Alone <amirhoseinsharify90@gmail.com>
2026-01-05 14:20:15 +0330

    add a health check route to /v1 route

--- main.go
+++ main.go
@@ -28,11 +28,15 @@ func main() {
 		AllowCredentials: false,
 		MaxAge:           300,
 	}))
+	v1Router := chi.NewRouter()
+	v1Router.Get("/ready", handleReadiness)
 
+	router.Mount("/v1", v1Router)
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
 	}
+
 	log.Printf("Server starting on Port %v", portString)
 	err := srv.ListenAndServe()
 	if err != nil {
